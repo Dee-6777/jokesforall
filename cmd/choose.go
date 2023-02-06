be7429fb175750cb438ebaa4552ee810724841a8
Deeptimayee Maharana <dmaharan@redhat.com>
2023-02-06 23:05:50 +0530

    comments added

--- cmd/choose.go
+++ cmd/choose.go
@@ -5,6 +5,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"strconv"
 
 	"github.com/spf13/cobra"
 )
@@ -14,11 +16,23 @@ var chooseCmd = &cobra.Command{
 	Use:   "choose",
 	Short: "A brief description of your command",
 	Long:  `Here you can choose any no of jokes you want to have!!!`,
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("choose called")
+		chooseJokes(args)
 	},
 }
 
+func chooseJokes(args []string) {
+	var num int
+	num, err := strconv.Atoi(args[0])
+	if err != nil {
+		log.Printf("Couldnot read the response. Error occured %v", err)
+	}
+	for i := 0; i < num; i++ {
+		getRandomJoke()
+	}
+}
 func init() {
 	rootCmd.AddCommand(chooseCmd)
 }
